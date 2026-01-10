0b950def4007cacd978b7fc85566e31993ce616a
Harry <harrycurtis@live.com>
2026-01-10 16:33:22 +0000

    feat: created hay wire logo component

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,32 +1,17 @@
-import { useSignal } from "@preact/signals";
 import { Head } from "fresh/runtime";
 import { define } from "../utils.ts";
-import Counter from "../islands/Counter.tsx";
+import HayWireLogo from "../components/HayWireLogo.tsx";
 
 export default define.page(function Home(ctx) {
-  const count = useSignal(3);
-
   console.log("Shared value " + ctx.state.shared);
 
   return (
-    <div class="px-4 py-8 mx-auto fresh-gradient min-h-screen">
+    <div class="mx-auto min-h-screen">
       <Head>
-        <title>Fresh counter</title>
+        <title>The Hay Wire</title>
       </Head>
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
+      <div class="md:max-w-screen mx-auto flex flex-col items-center justify-center">
+        <HayWireLogo />
       </div>
     </div>
   );
